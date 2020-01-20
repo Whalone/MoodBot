aa0ee5e9dbbf10661c8d8d43c2de854bc9373b1d
Whalone <469716638@qq.com>
2020-01-20 16:29:37 +0800

    修改两个icon

---
+++ mood/pages/bottle/bottle.js
@@ -0,0 +1,84 @@
+// pages/bottle/bottle.js
+
+const app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    openPicker: false,
+    needAnimation: false,
+    contentHeight: 0
+  },
+  onPickHeaderClick: function () {
+    this.setData({
+      openPicker: !this.data.openPicker,
+      needAnimation: true
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          //动态根据手机分辨率来计算内容的高度（屏幕总高度-顶部筛选栏的高度）
+          contentHeight: (res.windowHeight - 72 * res.screenWidth / 750)
+        });
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
